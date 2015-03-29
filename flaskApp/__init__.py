04dae30872ff2692e97eecaf425293b0031494ec
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-29 10:08:55 -0700

    Preparing for database

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flaskext.mysql import MySQL
+
+
+### SQL CONFIG ###
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '' # TODO Before putting this online put a sane password on the database
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+### END SQL CONFIG ###
 
-@app.route('/')
-def hello_world():
-    return 'Chips!'
+### ROUTES ###
+from flask import request
 
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    return "chips!"
+
+### END ROUTES ###
+
+
+if __name__ == "__main__":
     app.run()
